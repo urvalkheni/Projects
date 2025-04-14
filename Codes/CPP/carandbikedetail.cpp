26291b1597da8c988482a73690e4f59a6e8e56a6
Urval Kheni <kheniurval777@gmail.com>
2025-04-14 19:09:09 +0530

    Final Commit

--- Codes/CPP/carandbikedetail.cpp
+++ Codes/CPP/carandbikedetail.cpp
@@ -1,7 +1,71 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
-int main(){
-    cout<<"CAR AND BIKE DETAILES WILL BE DISPLAY IN FUTURE...!";
+class Vehicle {
+public:
+    string brand;
+    string model;
+    int year;
+
+    void getInput(string type) {
+        cout << "Enter " << type << " Brand: ";
+        getline(cin, brand);
+
+        cout << "Enter " << type << " Model: ";
+        getline(cin, model);
+
+        string yearStr;
+        cout << "Enter " << type << " Year: ";
+        getline(cin, yearStr);
+        year = stoi(yearStr); // Convert string to int
+    }
+
+    void saveToFile(string filename) {
+        ofstream out(filename, ios::app); // Append mode
+        out << brand << endl;
+        out << model << endl;
+        out << year << endl;
+        out << "-----" << endl; // Separator
+        out.close();
+    }
+
+    void displayFromFile(string filename, string type) {
+        ifstream in(filename);
+        if (!in) {
+            cout << type << " data not found.\n";
+            return;
+        }
+        cout << "\n--- " << type << " Details ---\n";
+        string line;
+        while (getline(in, line)) {
+            if (line == "-----") {
+                cout << "------------------------\n";
+                continue;
+            }
+            cout << line << endl;
+        }
+        in.close();
+    }
+};
+
+int main() {
+    Vehicle car, bike;
+
+    cout << "Enter Car Details:\n";
+    car.getInput("Car");
+
+    cout << "\nEnter Bike Details:\n";
+    bike.getInput("Bike");
+
+    // Save
+    car.saveToFile("cars.txt");
+    bike.saveToFile("bikes.txt");
+
+    // Display
+    car.displayFromFile("cars.txt", "Car");
+    bike.displayFromFile("bikes.txt", "Bike");
+
     return 0;
 }
